0858f20c4e926ab99c197677ecd87e4fc9e0a7a4
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-10-10 14:04:17 +0200

    initialize packages

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Justify text API
+# **Justify text API**
+
+## How to initialize the project
+
+``` bash
+pnpm install
+```
+
+## How to run the project
+
+``` bash
+pnpm run dev
+```
+
+### or
+
+``` bash
+pnpm run start
+```
+
+## How to test the project
+
+``` bash
+pnpm run test
+```
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
+});
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
